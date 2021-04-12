89af9fc618a3c4498e6516cd8cc61ab25a8632bd
SajibBarua-art <sajibbaruanstu@gmail.com>
2021-04-12 21:52:58 +0600

    appointment

--- src/components/Home/HeaderMain/HeaderMain.js
+++ src/components/Home/HeaderMain/HeaderMain.js
@@ -7,7 +7,7 @@ const HeaderMain = () => {
             <div className="col-md-4 offset-md-1">
                 <h1 style={{color:'#3A4256'}}>Your New Smile <br/> Starts Here</h1>
                 <p className="text-secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, magnam? Harum corporis odio illum rem?</p>
-                <button className="btn btn-primary">Get Appointment</button>
+                <button className="btn btn-brand">Get Appointment</button>
             </div>
             <div className="col-md-6">
                 <img src={chair} alt="" className="img-fluid"/>
